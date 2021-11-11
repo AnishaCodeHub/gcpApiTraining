06c02eb409625c82572d151b0cc2fa0a153d3ff0
Your Name <you@example.com>
2021-11-11 18:16:42 +0530

    first commit

---
+++ myapp.py
@@ -0,0 +1,46 @@
+# from flask import Flask
+# from datetime import datetime
+# import re
+
+# app = Flask(__name__)
+
+# @app.route("/")
+# def home():
+#     return "Hello, Flask!"
+
+# @app.route("/hello/<name>")
+# def hello_there(name):
+#     now = datetime.now()
+#     formatted_now = now.strftime("%A, %d %B, %Y at %X")
+
+#     # Filter the name argument to letters only using regular expressions. URL arguments
+#     # can contain arbitrary text, so we restrict to safe characters only.
+#     match_object = re.match("[a-zA-Z]+", name)
+
+#     if match_object:
+#         clean_name = match_object.group(0)
+#     else:
+#         clean_name = "Friend"
+
+#     content = "Hello there, " + clean_name + "! It's " + formatted_now
+#     return content
+
+
+
+from flask import Flask, jsonify, request
+#import os
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def bmi():
+    data = request.get_json()
+    height = data['height']
+    weight = data['weight']
+    bmi = weight/(height**2)
+    return(jsonify({"Body mass Index" : bmi}))
+
+if __name__ == '__main__':
+    app.run(debug=True,port=5000)#int(os.environ.get('PORT', 8080)))
+
+
